dfb9386c507a75e0d4ec361fa3ea96e23ef75bbf
lucasrbsouza <joselucastrbssilvasouza@gmail.com>
2026-03-26 09:01:44 -0300

    feat: define initial BPE training corpus

---
+++ bpe/corpus.py
@@ -0,0 +1,8 @@
+"""Training corpus for the BPE tokenizer experiment."""
+
+INITIAL_VOCAB: dict[str, int] = {
+    "l o w </w>": 5,
+    "l o w e r </w>": 2,
+    "n e w e s t </w>": 6,
+    "w i d e s t </w>": 3,
+}
